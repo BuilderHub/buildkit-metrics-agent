78c2e3473e4cc774d486e38474b99b7afc98c1d1
Zachary Spar <zspar@coreweave.com>
2026-05-07 14:19:25 -0700

    chore: nix package + container

--- flake.nix
+++ flake.nix
@@ -21,8 +21,47 @@
           ];
           targets = [ "x86_64-unknown-linux-musl" ];
         };
+
+        agent = pkgs.rustPlatform.buildRustPackage {
+          pname = "buildkit-metrics-agent";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = with pkgs; [ openssl ];
+        };
       in
       {
+        packages = {
+          default = agent;
+        } // pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+          containerImage = pkgs.dockerTools.buildLayeredImage {
+            name = "buildkit-metrics-agent";
+            contents = [
+              agent
+              pkgs.cacert
+            ];
+            config = {
+              Entrypoint = [ "${agent}/bin/buildkit-metrics-agent" ];
+              Env = [
+                "BUILDKIT_ADDR=unix:///run/buildkit/buildkitd.sock"
+                "METRICS_ADDR=0.0.0.0:9090"
+              ];
+              ExposedPorts = {
+                "9090/tcp" = { };
+              };
+              Labels = {
+                "org.opencontainers.image.source" = "https://github.com/builderhub/buildkit-metrics-agent";
+                "org.opencontainers.image.description" = "A lightweight Rust application that scrapes and exposes BuildKit metrics.";
+                "org.opencontainers.image.licenses" = "MIT";
+                "org.opencontainers.image.authors" = "BuilderHub";
+                "org.opencontainers.image.url" = "https://github.com/builderhub/buildkit-metrics-agent";
+                "org.opencontainers.image.documentation" = "https://github.com/builderhub/buildkit-metrics-agent/blob/main/README.md";
+                "org.opencontainers.image.vendor" = "BuilderHub";
+              };
+            };
+          };
+        };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             toolchain
